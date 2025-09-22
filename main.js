362cd3c5ea23aa1e579cb0567b7edfe0f9185b53
Nsuku-Goldwin <nsukugoldwin1234@gmail.com>
2025-09-22 11:28:16 +0200

    Refactor calculator button handlers to use a single function
    Replaced individual button handler functions with a unified appendToDisplay(input) function for all calculator buttons. Updated index.html to use this new function, simplifying main.js and reducing code duplication. Parenthesis buttons are now disabled.

--- main.js
+++ main.js
@@ -1,79 +1,13 @@
 let screen = "";
-function printScreen() {
+function appendToDisplay(input) {
+  screen += input;
   document.getElementById("screen-p").innerHTML = screen;
 }
-function one() {
-  screen += 1;
-  printScreen();
-}
-function two() {
-  screen += 2;
-  printScreen();
-}
-function three() {
-  screen += 3;
-  printScreen();
-}
-function four() {
-  screen += 4;
-  printScreen();
-}
-function five() {
-  screen += 5;
-  printScreen();
-}
-function six() {
-  screen += 6;
-  printScreen();
-}
-function seven() {
-  screen += 7;
-  printScreen();
-}
-function eight() {
-  screen += 8;
-  printScreen();
-}
-function nine() {
-  screen += 9;
-  printScreen();
-}
-function zero() {
-  screen += 0;
-  printScreen();
-}
+
 function clearAll() {
   screen = "";
-  printScreen();
+  document.getElementById("screen-p").innerHTML = screen;
   document.getElementById("screen2").innerHTML = "";
-}
-function paranth1() {
-  screen += "(";
-  printScreen();
-}
-function paranth2() {
-  screen += ")";
-  printScreen();
-}
-function devide() {
-  screen += "/";
-  printScreen();
-}
-function multiply() {
-  screen += "x";
-  printScreen();
-}
-function add() {
-  screen += "+";
-  printScreen();
-}
-function subtract() {
-  screen += "-";
-  printScreen();
-}
-function remainder() {
-  screen += "%";
-  printScreen();
 }
 function equals() {
   try {
